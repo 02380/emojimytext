be53103c17f31b0a4b09ad2cf4b7253050898afb
02380 <49149558+02380@users.noreply.github.com>
2020-07-07 22:41:42 +0100

    polyfill

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,16 @@
 		}
 		return loc_text;
 	}
-
+	if (!String.prototype.replaceAll) {
+	  String.prototype.replaceAll = function(find, replace) {
+	    let s = '', index, next;
+	    while (~(next = this.indexOf(find, index))) {
+	      s += this.substring(index, next) + replace;
+	      index = next + find.length;
+	    }
+	    return s + this.substring(index);
+	  };
+	}
 </script>
 
 <main in:fade>
